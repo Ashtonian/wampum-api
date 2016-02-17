cb85669f1dc50c68a67b0feead375c976b6f5496
Ashton Kinslow <github@ashtonkinslow.com>
2016-02-16 22:06:24 -0600

    adding initial postgres db test

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      res.send(JSON.stringify(row));
+    });
+});
+
 });
 
 app.listen(port, function () {

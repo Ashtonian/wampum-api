51c5ded74f1cc30995d5d66e2137af423b312402
Ashton Kinslow <github@ashtonkinslow.com>
2016-02-16 21:25:51 -0600

    cleaning house

--- models/user.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Ashtonian on 10/31/15.
- */
-var User = (function () {
-    function User() {
-    }
-    return User;
-})();
-//# sourceMappingURL=user.js.map
--- models/user.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Ashtonian on 10/31/15.
- */
-
-class User {
-    UserId : string;
-    FirstName : string;
-    LastName : string;
-}

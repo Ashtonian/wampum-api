3f94bd8e133de3e0a8b9b18cbd9f520cf1b970ba
Ashton Kinslow <github@ashtonkinslow.com>
2016-02-29 21:52:37 -0600

    adding image insert functionality, updating naming convention to be consistent with postgres

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ var pg = require('pg').native;
 var aws = require('aws-sdk');
 var app = express();
 
+// TODO: move routes to seperate place?
+// TODO: create controllers?
+// TODO: create data service for each resource?
+// TODO: folder structure
+// TODO: response status code goes elsewhere?
+// TODO: response content-type elsewhere?
+// TODO: response content-length elsewhere?
+// TODO: async/promises/events for db response?
+// TODO: CREATE SERVICES BEFORE YOU LOOSE YOUR MIND
+// TODO: create integration tests?
+// TODO: create mocks?
+// TODO: named postgres query parameters?
+// TODO: create postgres sql sprocs/prepared sql query?
+// TODO: error handleing and 500 status code?
+// TODO: version and base api route?
+// TODO: if _id path is not found should return 404?
+// TODO: using UUID for PK and postgresSql?
+// TODO: fix route nesting?
+// TODO: bytea image
+// TODO: multiple images pert barter-item/move images to image table
+// TODO: add swagger to api
+// TODO: user auth
+// TODO: get all sql errors ever?
+// TODO: catch all outgoing response errors?
+// TODO: sequential UUID? quessable uuid? v1 vs v4?
+
 var logger = function(req, res, next) {
   console.log("request: " + req.originalUrl);
   for (var propName in req.body) {
@@ -20,7 +46,7 @@ var logger = function(req, res, next) {
   var host = req.get('host');
   console.log("host " + host);
   var origin = req.get('origin');
-console.log("origin " + origin);
+  console.log("origin " + origin);
 
   next(); // Passing the request to the next handler in the stack.
 
@@ -68,37 +94,11 @@ function getSignedUrl(fileName, fileExtension) {
     Bucket: S3_BUCKET,
     Key: fileName,
     Expires: 10,
-    ContentType: GetMimeTypeFromExtension(fileExtension) ,
+    ContentType: GetMimeTypeFromExtension(fileExtension),
     ACL: 'public-read'
   };
   return s3.getSignedUrl('putObject', s3_params);
 }
-
-
-
-// TODO: move routes to seperate place?
-// TODO: create controllers?
-// TODO: create data service for each resource?
-// TODO: folder structure
-// TODO: response status code goes elsewhere?
-// TODO: response content-type elsewhere?
-// TODO: response content-length elsewhere?
-// TODO: async/promises/events for db response?
-// TODO: create integration tests?
-// TODO: create mocks?
-// TODO: named postgres query parameters?
-// TODO: create postgres sql sprocs/prepared sql query?
-// TODO: error handleing and 500 status code?
-// TODO: version and base api route?
-// TODO: if _id path is not found should return 404?
-// TODO: using UUID for PK and postgresSql?
-// TODO: fix route nesting?
-// TODO: bytea image
-// TODO: multiple images pert barter-item/move images to image table
-// TODO: add swagger to api
-// TODO: user auth
-// TODO: get all sql errors ever?
-// TODO: catch all outgoing response errors?
 
 var userRouter = express.Router();
 userRouter.route('/')
@@ -153,12 +153,11 @@ userRouter.route('/:_id')
 
 var barterItemRouter = express.Router();
 barterItemRouter.route('/')
-
-.get(function(req, res) {
+  .get(function(req, res) {
     // TODO: optional parameters use samse query
     if (req.query.currentUser) {
       pg.connect(connStr, function(err, client, done) {
-        client.query('SELECT * FROM public.barteritem where _userid = ($1)', [defaultUserId], function(err, result) {
+        client.query('SELECT * FROM public.barter_item where _user_id = ($1)', [defaultUserId], function(err, result) {
           if (err)
             res.end('an error occured' + err);
           done();
@@ -173,7 +172,7 @@ barterItemRouter.route('/')
       });
     } else {
       pg.connect(connStr, function(err, client, done) {
-        client.query('SELECT * FROM public.barteritem', function(err, result) {
+        client.query('SELECT * FROM public.barter_item', function(err, result) {
           if (err)
             res.end('an error occured' + err);
           done();
@@ -194,14 +193,14 @@ barterItemRouter.route('/')
     // TODO: make sure that DeviceFileUrl is valid?
 
     var barterItem = req.body;
-    barterItem._userid = defaultUserId;
-    barterItem._id = uuid.v1();
+    barterItem._user_id = defaultUserId;
+    barterItem._id = uuid.v4();
 
     // Insert base item meta data
     pg.connect(connStr, function(err, client, done) {
       if (err)
         console.log('pg connect error: ' + err);
-      client.query('INSERT INTO public.barteritem (_id,_userid,title,description,image) VALUES($1::uuid,$2::uuid,$3,$4,$5)', [barterItem._id, barterItem._userid, barterItem.title, barterItem.description, ""], function(err, result) {
+      client.query('INSERT INTO public.barter_item (_id,_user_id,title,description) VALUES($1::uuid,$2::uuid,$3,$4)', [barterItem._id, barterItem._user_id, barterItem.title, barterItem.description], function(err, result) {
         if (err) {
           console.log('insert error: ' + err);
           res.end('an error occured' + err);
@@ -215,7 +214,13 @@ barterItemRouter.route('/')
 
           var fileUrl = barterItem.images[0];
           var fileType = fileUrl.substr(fileUrl.lastIndexOf('.'));
-          var fileName = uuid.v1() + fileType;
+          var imageId = uuid.v4();
+          var fileName = imageId + fileType;
+          InsertImage({
+            _id: imageId,
+            _barter_item_id: barterItem._id,
+            file_extension: fileType
+          });
 
           var signedUrl = getSignedUrl(fileName, fileType);
           var return_data = {
@@ -235,9 +240,23 @@ barterItemRouter.route('/')
     res.send('item is suppossed to be updated if this was implemented.');
   });
 
+
+function InsertImage(image) {
+  pg.connect(connStr, function(err, client, done) {
+    if (err)
+      console.log('pg connect error: ' + err);
+    client.query('INSERT INTO public.barter_item_image (_id,_barter_item_id,file_extension) VALUES($1::uuid,$2::uuid,$3)', [image._id, image._barter_item_id, image.file_extension], function(err, result) {
+      if (err) {
+        console.log('insert error: ' + err);
+      }
+      done();
+    });
+  });
+}
+
 barterItemRouter.route('/recommendations/').get(function(req, res) {
   pg.connect(connStr, function(err, client, done) {
-    client.query('SELECT * FROM public.barteritem', function(err, result) {
+    client.query('SELECT * FROM public.barter_item', function(err, result) {
       if (err)
         res.end('an error occured' + err);
       done();
@@ -255,7 +274,7 @@ barterItemRouter.route('/recommendations/').get(function(req, res) {
 barterItemRouter.route('/:_id')
   .get(function(req, res) {
     pg.connect(connStr, function(err, client, done) {
-      client.query('SELECT * FROM public.barteritem WHERE _id = $1::uuid', [req.params._id], function(err, result) {
+      client.query('SELECT * FROM public.barter_item WHERE _id = $1::uuid', [req.params._id], function(err, result) {
         if (err)
           res.end('an error occured' + err);
         done();
